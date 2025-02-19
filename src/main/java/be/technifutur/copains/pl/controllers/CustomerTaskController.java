762257eed56d8b68923a47ce8ab75c1abae04af0
Veysel Calisgan <veyselcalisgan@live.be>
2025-02-19 14:23:14 +0100

    third commit

--- src/main/java/be/technifutur/copains/pl/controllers/CustomerTaskController.java
+++ src/main/java/be/technifutur/copains/pl/controllers/CustomerTaskController.java
@@ -5,19 +5,32 @@ import be.technifutur.copains.bll.services.CustomerTaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/customertask")
+@RequestMapping("/")
 @SessionAttributes("id")
 public class CustomerTaskController {
 
     private final CustomerTaskService customerTaskService;
 
+    @GetMapping
     public String getAllCustomerTasks(Model model){
-        model.addAttribute("customerTask", customerTaskService.getAllCustomerTask());
-        return "customertask/index";
+        model.addAttribute("customerTasks", customerTaskService.getAllCustomerTask());
+        return "/index";
     }
+
+    @GetMapping("/customer-task-details/{id}")
+    public String getCustomerTaskById(
+            Model model,
+            @PathVariable Long id
+    ){
+        model.addAttribute("customerTaskDetails", customerTaskService.getCustomerTaskById(id));
+        return "pages/customerTaskDetails";
+    }
+
 }
--- src/main/java/be/technifutur/copains/utils/DataInitializer.java
+++ src/main/java/be/technifutur/copains/utils/DataInitializer.java
@@ -9,6 +9,8 @@ import be.technifutur.copains.dl.entities.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 import static be.technifutur.copains.dl.enums.UserRole.CUSTOMER;
 
 @Component
@@ -30,11 +32,57 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User("TheBigPush", "1", CUSTOMER, true);
+        if(customerTaskRepository.count() == 0) {
 
-        userRepository.save(user);
+            List<User> users = userRepository.saveAll(List.of(
+                    new User("TheBigPush", "1", CUSTOMER, true),
+                    new User("KendrickLamar", "1", CUSTOMER, true),
+                    new User("2Pac", "1", CUSTOMER, true),
+                    new User("Big", "1", CUSTOMER, true),
+                    new User("Jennifer", "1", CUSTOMER, true),
+                    new User("Damien", "1", CUSTOMER, true),
+                    new User("Nico", "1", CUSTOMER, true),
+                    new User("Thomas", "1", CUSTOMER, true),
+                    new User("Titou", "1", CUSTOMER, true),
+                    new User("Dylan", "1", CUSTOMER, true),
+                    new User("Claire", "1", CUSTOMER, true),
+                    new User("Thibault", "1", CUSTOMER, true),
+                    new User("Vincent", "1", CUSTOMER, true),
+                    new User("Seba", "1", CUSTOMER, true),
+                    new User("Jolan", "1", CUSTOMER, true),
+                    new User("Seb", "1", CUSTOMER, true),
+                    new User("Danjel", "1", CUSTOMER, true),
+                    new User("Veys", "1", CUSTOMER, true),
+                    new User("Barnabé", "1", CUSTOMER, true),
+                    new User("Yannick", "1", CUSTOMER, true)
 
-        CustomerTask customerTask = new CustomerTask("Babysitter", "I can take care of your babies", "meganFox.png", 1000, user);
-        customerTaskRepository.save(customerTask);
+            ));
+
+
+            List<CustomerTask> tasks = List.of(
+                    new CustomerTask("Babysitter", "I can take care of your babies", "1.jpg", 1000, users.get(0)),
+                    new CustomerTask("Garden", "I can mow your grass", "2.jpg", 100, users.get(1)),
+                    new CustomerTask("Heat", "I can repair your heat", "3.jpg", 500, users.get(2)),
+                    new CustomerTask("Electrician", "I can change your lamp", "4.jpg", 10, users.get(3)),
+                    new CustomerTask("Smartphone", "My smartphone is broke", "5.jpg", 200, users.get(4)),
+                    new CustomerTask("Cofee Machine broken", "Someone can repair my cofee machine please", "6.jpg", 10, users.get(5)),
+                    new CustomerTask("old guy", "I need help for buy groceries", "7.jpg", 15, users.get(6)),
+                    new CustomerTask("old lady", "I make chrismas cake", "8.jpg", 70, users.get(7)),
+                    new CustomerTask("school", "I can take your childs to school", "9.jpg", 15, users.get(8)),
+                    new CustomerTask("carwash", "I wash car at your place", "10.jpg", 50, users.get(9)),
+                    new CustomerTask("clean home", "I can clean your home", "11.jpg", 100, users.get(10)),
+                    new CustomerTask("music", "I need somoene to build a mix", "12.jpg", 200, users.get(11)),
+                    new CustomerTask("car broken", "Somoene does know about cars?", "13.jpg", 200, users.get(12)),
+                    new CustomerTask("tires", "I can change the tires of your cars", "14.jpg", 40, users.get(13)),
+                    new CustomerTask("dress", "did someoene can repair my sweather?", "15.jpg", 5, users.get(14)),
+                    new CustomerTask("barmaid", "I perform for weddings", "16.jpg", 500, users.get(15)),
+                    new CustomerTask("cameraman", "the best videos for your parties", "17.jpg", 1500, users.get(16)),
+                    new CustomerTask("Striper", "did somoene know a male stripper for birthday", "18.jpg", 150, users.get(17)),
+                    new CustomerTask("kitchen", "I make delicious foods", "19.jpg", 15, users.get(18)),
+                    new CustomerTask("gaming", "wanted friend for gaming night", "20.jpg", 0, users.get(19))
+            );
+            customerTaskRepository.saveAll(tasks);
+
+        }
     }
 }
